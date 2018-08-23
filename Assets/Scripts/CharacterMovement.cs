5d5acdf1d86d68113b886b9f82dc5fd204649ed8
scazzman99 <scazzman99@gmail.com>
2018-08-23 15:40:19 +1000

    Added a new scene for raincollision

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterMovement : MonoBehaviour {
+
+    #region Variables
+    public Rigidbody playerRigid;
+    public float speed = 4f;
+    public float grav = 20f;
+    public float jumpSpeed = 8f;
+    public float rayDistance = 1f;
+    #endregion
+    // Use this for initialization
+    void Start () {
+        playerRigid = GameObject.Find("Player").GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float inputH = Input.GetAxis("Horizontal") * speed;
+
+        Vector3 moveDir = new Vector3(inputH, 0, 0);
+        Vector3 force = new Vector3(moveDir.x, playerRigid.y, 0);
+
+        if(Input.GetButton("Jump") && isGrounded())
+        {
+            force.y = jumpSpeed;
+        }
+
+        playerRigid.velocity = force;
+    }
+
+    bool isGrounded()
+    {
+        Ray groundRay = new Ray(playerRigid.position, Vector3.down);
+        RaycastHit hit;
+        if(Physics.Raycast(groundRay, out hit, rayDistance))
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/RainDamage.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainDamage : MonoBehaviour {
+
+    #region Vars
+    public Rigidbody playerRigid;
+    public float playerSize = 100f;
+    public float maxSize = 100f;
+    public float rainDamage = 4f;
+    public bool inCover;
+    #endregion
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray roofCheck = new Ray(playerRigid.position, Vector3.up); //will check for anything above the player
+        RaycastHit hit;
+        if(Physics.Raycast(roofCheck, out hit, 1000f))
+        {
+            inCover = true;
+        } else
+        {
+            inCover = false;
+        }
+	}
+
+
+
+
+    private void LateUpdate()
+    {
+        if(inCover == false)
+        {
+            playerSize -= rainDamage * Time.deltaTime; //Deplete Size over time
+            Debug.Log("Not in cover: DAMAGE TAKEN");
+        }
+    }
+
+
+
+
+}
