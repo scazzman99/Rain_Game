47bd4e3dcb69aefbc83e0e7df77235fb3d24c260
scazzman99 <scazzman99@gmail.com>
2018-09-10 13:36:26 +1000

    Change 13
    Added dripping capsule that deals damage on touch, designing a small room around it. Gonna do more soon

---
+++ Assets/Scripts/DestroyDelay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyDelay : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            RainDamage playerHp = GameObject.FindGameObjectWithTag("Player").GetComponent<RainDamage>();
+            playerHp.playerSize -= 30f;
+            Destroy(gameObject);
+            
+        }
+
+        if (other.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Drip.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drip : MonoBehaviour {
+
+    public float vel;
+    public Vector3 dir;
+    public GameObject drip;
+    public RainDamage playerHP;
+    public GameObject dripSpawn;
+    public bool isDrip;
+    public float dripRate;
+
+
+	// Use this for initialization
+	void Start () {
+        dir = Vector3.down;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isDrip)
+        {
+            GameObject waterDrop = Instantiate(drip, dripSpawn.transform.position, dripSpawn.transform.rotation);
+            Rigidbody waterDropR = waterDrop.GetComponent<Rigidbody>();
+            waterDropR.AddForce(dir);
+            isDrip = false;
+            StartCoroutine(DropWaterSet());
+        }
+
+	}
+
+    public IEnumerator DropWaterSet()
+    {
+        yield return new WaitForSecondsRealtime(dripRate);
+        isDrip = true;
+    }
+}
